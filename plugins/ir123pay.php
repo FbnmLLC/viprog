f18e4a16b08b03550b03b93bcad9d55912c09659
Hojjat Ali Mohammadi <safeith@riseup.net>
2017-10-19 15:25:43 +0330

    Fix functions

--- plugins/ir123pay.php
+++ plugins/ir123pay.php
@@ -20,19 +20,17 @@ function gateway__ir123pay( $data ) {
 	$response = create( $merchant_id, $amount, $callback_url );
 	$result   = json_decode( $response );
 	if ( $result->status ) {
-		$update['payment_rand'] = $result->RefNum;
-		$sql                    = $db->queryUpdate( 'payment', $update, 'WHERE `payment_rand` = "' . $data['invoice_id'] . '" LIMIT 1' );
-		$db->execute( $sql );
+		$update[ payment_rand ] = $result->RefNum;
+		$sql                    = $db->prepare( "UPDATE `payment` SET `payment_rand` = ? WHERE `payment_rand` = ? LIMIT 1" );
+		$sql->execute( array( $result->RefNum, $data[ invoice_id ] ) );
 		$go = $result->payment_url;
 		redirect_to( $go );
 	} else {
-		$data['title']   = 'خطای سیستم';
-		$data['message'] = '<span style="color: #ff0000">پیام سیستم : </span>' . $result->message . '<br /><a href="index.php" class="button">بازگشت</a>';
-		$query           = 'SELECT * FROM `config` WHERE `config_id` = "1" LIMIT 1';
-		$conf            = $db->fetch( $query );
-		$smarty->assign( 'config', $conf );
+		$data[ title ]   = 'پیام سیستم';
+		$data[ message ] = '<font color="#ff0000">' . $result->message . '</font><br /><a href="index.php" class="button">بازگشت</a>';
 		$smarty->assign( 'data', $data );
 		$smarty->display( 'message.tpl' );
+		exit();
 	}
 }
 
@@ -47,8 +45,9 @@ function callback__ir123pay( $data ) {
 		$response = verify( $merchant_id, $RefNum );
 		$result   = json_decode( $response );
 		if ( $result->status ) {
-			$sql     = 'SELECT * FROM `payment` WHERE `payment_rand` = "' . $RefNum . '" LIMIT 1;';
-			$payment = $db->fetch( $sql );
+			$sql     = "SELECT * FROM `payment` WHERE `payment_rand` = '$RefNum' LIMIT 1";
+			$payment = $db->query( $sql )->fetch();
+
 			if ( $payment ) {
 				if ( $payment['payment_amount'] == $result->amount ) {
 					if ( $payment['payment_status'] == 1 ) {
